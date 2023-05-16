15ddf687ee9bc8de14041e9fdfe186fcaf1365d4
Nadau07 <noah.liebert@outlook.fr>
2023-05-16 14:37:27 +0200

    Afficher les img avec l'API

--- FrontEnd/api.js
+++ FrontEnd/api.js
@@ -5,19 +5,19 @@ const baseUrl = 'http://localhost:5678/api';
 async function getWorks(){
 
     const response = await fetch ('http://localhost:5678/api/works')
-    console.log(response)
     const data = await response.json()
     console.log(data)
     return data
+
+
 }
 
 
 async function Categories(){
     const response2 = await fetch ('http://localhost:5678/api/categories')
-    console.log(response2)
     const data2 = await response2.json()
-    console.log(data2)
     return data2
 
     document.querySelector('#filter').src =data2.url
 }
+
--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,7 +1,21 @@
-async function init(){
-    console.log('init')
-   await getWorks()
-   await Categories()
-}
-init()
 
+//Afficher les images : 
+async function displayWorks(){
+const gallery = document.querySelector(".gallery");
+const works = await getWorks();
+
+works.forEach(works => {
+
+    const figureElement = document.createElement('figure');
+    const workImg = document.createElement('img');
+    workImg.src = works.imageUrl;
+    const workId = document.createElement('figcaption');
+    workId.innerText = works.title;
+
+    gallery.appendChild(figureElement);
+    figureElement.appendChild(workId);
+    figureElement.appendChild(workImg);
+});
+}
+
+displayWorks();
