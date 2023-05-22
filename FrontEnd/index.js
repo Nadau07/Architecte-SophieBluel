f090f6fd410e2992bbef4947063156431e0673b9
Nadau07 <noah.liebert@outlook.fr>
2023-05-22 12:07:34 +0200

    Filtrage des images par categories OK

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,26 +1,28 @@
 //AFFICHER la partie filtre:
 async function displayCategories(){
-    const filter = document.querySelector('.filter');
-    const categories = await Categories();
-    categories.unshift({ name: 'Tous' }); //Ajout "Tous" dans le tableau
+  const filter = document.querySelector('.filter');
+  const categories = await Categories();
+  categories.unshift({ name: 'Tous' }); //Ajout "Tous" dans le tableau
 
-    categories.forEach(category => {
-        const Element = document.createElement('p');
-        Element.innerText = category.name;
-        Element.addEventListener('click', () => filtrage(category.name, works)); 
-        // Ajout du gestionnaire d'événements au clic pour filtrer
 
-        filter.appendChild(Element);
-    })
+  //Boucle pour creer les noms de categories
+  categories.forEach(category => {
+      const Element = document.createElement('p');
+      Element.innerText = category.name;
+      Element.addEventListener('click', () => {
+        displayWorks(category.name); //Ajout click sur les categories
+      });
+      filter.appendChild(Element); //Ajout chaque categories dans filter
+  });
 }
 displayCategories(); //execute l'affichage des boutons
 
+const categoryName = 'Tous';
 
-//AFFICHER les images : 
-async function displayWorks(){
+//AFFICHER les images en fonction des categories : 
+async function displayWorks(categoryName){
 const gallery = document.querySelector(".gallery");
 const works = await getWorks();
-
 
 //TRIER LES ELEMENTS PAR CATEGORIES (avec les id):
 
@@ -36,35 +38,31 @@ const idHotelBar = [3,10,11];
 const filterHotelBar = works.filter(work => idHotelBar.includes(work.id));
 console.log(filterHotelBar);
 
+let filteredWorks = works;
 
-    works.forEach(works => {
-    
-        const figureElement = document.createElement('figure');
-        const workImg = document.createElement('img');
-        workImg.src = works.imageUrl;
-        const workId = document.createElement('figcaption');
-        workId.innerText = works.title;
-    
-        gallery.appendChild(figureElement);
-        figureElement.appendChild(workImg);
-        figureElement.appendChild(workId);
-    });
-    }
-    
+if (categoryName === 'Tous') {
+  filteredWorks = works;
+} else if (categoryName === 'Objets') {
+  filteredWorks = works.filter(work => idObjets.includes(work.id));
+} else if (categoryName === 'Appartements') {
+  filteredWorks = works.filter(work => idAppart.includes(work.id));
+} else if (categoryName === 'Hotels & restaurants') {
+  filteredWorks = works.filter(work => idHotelBar.includes(work.id));
+}
+
+gallery.innerHTML = '';
+  filteredWorks.forEach(works => {
+  
+      const figureElement = document.createElement('figure');
+      const workImg = document.createElement('img');
+      workImg.src = works.imageUrl;
+      const workId = document.createElement('figcaption');
+      workId.innerText = works.title;
+  
+      gallery.appendChild(figureElement);
+      figureElement.appendChild(workImg);
+      figureElement.appendChild(workId);
+  });
+  }
+  
 displayWorks(); // execute l'affichage des images
-
-
-//CREER LA FONCTION FILTRAGE :
-
-/* A RETRAVAILLER :async function filtrage(categoryName, works){
-    if (categoryName === 'Tous') {
-        filteredWorks = works;
-      } else if (categoryName === 'Objets') {
-        filteredWorks = works.filter(work => idObjets.includes(work.id));
-      } else if (categoryName === 'Appart') {
-        filteredWorks = works.filter(work => idAppart.includes(work.id));
-      } else if (categoryName === 'HotelBar') {
-        filteredWorks = works.filter(work => idHotelBar.includes(work.id));
-      }
-    console.log(filtrage);
-}*/
