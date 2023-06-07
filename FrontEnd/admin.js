7d4e651865195e29be9bc7f35fa6046b3fe77fe8
Nadau07 <noah.liebert@outlook.fr>
2023-06-07 16:34:24 +0200

    Ajout de la galerie dans la modale

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -6,20 +6,41 @@ const ajoutImg = document.querySelector(".modal-ajout");
 const suppImg = document.querySelector(".modal-supp");
 const btnModal = document.querySelector(".modal-btn");
 
-
+//AFFICHER-MASQUER LA MODALE//
 function afficherModal(){
-    modal.style.display ="flex";
+    modal.style.display ="block";
 }
-
 function masquerModal(){
     modal.style.display = "none";
 }
-
 btnModal.addEventListener("click", afficherModal);
 closeModal.addEventListener("click", masquerModal);
 
-//FAIRE APPARAITRE: LogOut, Barre d'edition et bouton modifier//
+//IMAGES dans modale//
+async function displayModal(works){
+    const gallery2 = document.querySelector(".gallery2");
+    gallery2.innerHTML="";
+    works.forEach((work) => {
+      const figureElement = document.createElement("figure");
+      const workImg = document.createElement("img");
+      workImg.src = work.imageUrl;
+      const workP = document.createElement("p");
+      workP.innerText = "éditer";
+  
+      gallery2.appendChild(figureElement);
+      figureElement.appendChild(workImg);
+      figureElement.appendChild(workP);
+    });
+  }
+  async function init(){
+    const works = await getWorks();
+    displayModal(works);
+  }
+  
+  init();
 
+//FAIRE APPARAITRE: LogOut, Barre d'edition et bouton modifier//
+function updateLogin(){
 const logStatus = document.querySelector(".login-link");
 //console.log(logStatus)
 const token = localStorage.getItem("token");
@@ -27,21 +48,22 @@ const token = localStorage.getItem("token");
 const boutonModifierModal = document.querySelector(".modal-btn");
 const barreEdition = document.querySelector(".barreEdition");
 const boutonModifier2 =document.querySelector(".btn-modifier");
+const filter = document.querySelector(".filter");
 
 const loginOk =() => (token ? true : false);
 
-if(loginOk){
-    logStatus.innerHTML ="Logout";
-    boutonModifierModal.style.display="block";
+if(loginOk()){
+    logStatus.innerHTML ="logout";
+    boutonModifierModal.style.display="block"; 
       barreEdition.style.display="block";
       boutonModifier2.style.display="block";
-      
-}else{
-    window.location.href="./index.html";
-    logStatus.innerText ="Login";
-    boutonModifierModal.style.display="none";
-      barreEdition.style.display="none";
-      boutonModifier2.style.display="none";
+      filter.style.display ="none"; 
+      //cliquer sur Logout pour se déconnecter
+      logStatus.addEventListener("click", ()=>{
+        localStorage.clear("token");
+        console.log("déconnecter");
+        window.location.href ="./index.html";
+      });
+    }};
 
-};
-
+updateLogin();
