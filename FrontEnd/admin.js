c8924558c60d7eba069d0e8b9c211b7073c4964a
Nadau07 <noah.liebert@outlook.fr>
2023-06-15 19:08:14 +0200

    Avancement du projet : Upload new img

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -10,7 +10,11 @@ const imageUpload = document.querySelector("#image_uploads");
 const imgPrevisuel = document.querySelector("#icone_previsuel");
 const btnAjoutImgModal2 = document.querySelector(".btn-ajout-photo");
 const gallery = document.querySelector(".gallery");
-const figureElement = document.createElement("figure");
+const figureGallery1 = document.querySelector("figure1");
+const boutonValider = document.querySelector(".modal2-valider");
+const titreModal2 = document.querySelector(".titreModal2");
+const closeModal2 = document.querySelector(".modal2-close");
+
 
 //console.log(imageUpload);
 //console.log(icone_previsuel);
@@ -18,24 +22,29 @@ const figureElement = document.createElement("figure");
 
  ///////// AFFICHAGE DES MODALES /////////
 
-//AFFICHER-MASQUER LA MODALE 1 //
+//AFFICHER-MASQUER LA MODALE 1 ET 2 //
 function afficherModal() {
   modal.style.display = "block";
 };
 function masquerModal() {
   modal.style.display = "none";
 };
+function masquerModal2(){
+  modal2.style.display="none";
+}
 btnModal.addEventListener("click", afficherModal);
 closeModal.addEventListener("click", masquerModal);
+closeModal2.addEventListener("click", masquerModal2);
 
 // MODALE 1 : afficher les projets //
 async function displayModalImg(works) {
   const gallery2 = document.querySelector(".gallery2");
   gallery2.innerHTML = "";
   works.forEach((work) => {
-    const figureElement = document.createElement("figure");
+    const figureElement2 = document.createElement("figure");
     const workImg = document.createElement("img");
     workImg.src = work.imageUrl;
+    workImg.dataset.indexId = work.id;
     const workP = document.createElement("p");
     workP.innerText = "éditer";
     const iconeDelet = document.createElement("div");
@@ -44,13 +53,16 @@ async function displayModalImg(works) {
 
     //Supprimer un element en cliquant sur l'icone corbeille//
     iconeDelet.addEventListener("click", () => {
-      figureElement.remove();
+      figureElement2.remove();
+      figureGallery1.remove();
     });
     //// Supprimer toute la gallerie ( modale 1)////
     suppImg.addEventListener("click", ()=>{
       gallery2.innerHTML="";
       gallery.innerHTML=""; //supp aussi la gallerie sur index.html//
     });
+
+    
     const firstFigure = document.querySelector(".gallery2 figure:first-child");
     if (firstFigure && !firstFigure.querySelector(".iconePosition")) {
       const iconePosition = document.createElement("div");
@@ -59,12 +71,60 @@ async function displayModalImg(works) {
       firstFigure.appendChild(iconePosition);
     }
 
-    gallery2.appendChild(figureElement);
-    figureElement.appendChild(workImg);
-    figureElement.appendChild(workP);
-    figureElement.appendChild(iconeDelet);
+    gallery2.appendChild(figureElement2);
+    figureElement2.appendChild(workImg);
+    figureElement2.appendChild(workP);
+    figureElement2.appendChild(iconeDelet);
   });
 };
+
+
+//Ajouter une image dans la modale et l'index//
+
+imageUpload.addEventListener("change", (event)=>{
+  const selectedFile = event.target.files[0];
+  window.selectedFile = selectedFile;
+  affichagePrevisuel(selectedFile);
+});
+
+function affichagePrevisuel(selectedFile){
+ 
+  const newImage = document.createElement("img");
+  newImage.src = URL.createObjectURL(selectedFile);
+  newImage.classList.add("modal2NewImg");
+  imgPrevisuel.innerHTML="";
+  imgPrevisuel.appendChild(newImage);
+
+}
+
+
+boutonValider.addEventListener("click", ()=>{
+  if (window.selectedFile){
+    
+      const title = document.querySelector(".titreModal2").value;
+      if (title === ""){
+      const messageErreur = document.querySelector(".error-title");
+      messageErreur.style.display="block";
+      return;
+    }
+   
+    addElementWithPhoto(window.selectedFile, title);
+    }
+  }
+);
+
+function addElementWithPhoto(photoFile, title){
+  const newElement = document.createElement("div");
+  const newImage = document.createElement("img");
+  newImage.src = URL.createObjectURL(photoFile);
+  const ajoutTitre = document.querySelector("figcaption");
+  ajoutTitre.textContent = title;
+  newElement.appendChild(newImage);
+  newElement.appendChild(ajoutTitre);
+  gallery.appendChild(newElement); 
+};
+
+
 
     
  
@@ -97,6 +157,7 @@ precedent.addEventListener("click", ()=>{
   
   });
 };
+
 
 
 
